e30680ebdb63c8a8489ce9f8a7b84787a5e2e849
Jeff Cincotta <jcincotta@zipcar.com>
2018-11-21 15:12:03 -0500

    rabbitmq practice

---
+++ src/main/java/com/rabbitmq/Recieve.java
@@ -0,0 +1,33 @@
+package com.rabbitmq;
+
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+
+public class Recieve {
+        private final static String QUEUE_NAME = "hello";
+
+        public static void main(String[] argv) throws IOException, InterruptedException, TimeoutException {
+
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.setHost("localhost");
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+            Consumer consumer = new DefaultConsumer(channel) {
+                @Override
+                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                    String message = new String(body, StandardCharsets.UTF_8 );
+                    System.out.println(" [x] Received '" + message + "'");
+                }
+            };
+            channel.basicConsume(QUEUE_NAME, true, consumer);
+        }
+
+
+}
